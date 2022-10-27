106b48ed80013803f86746802b5d7fd630fe65a4
Cau <cauindo113@gmail.com>
2022-10-27 17:22:31 +0700

    fix img change

--- src/component/DetailSong.js
+++ src/component/DetailSong.js
@@ -8,10 +8,10 @@ export default function DetailSong() {
        <h2 className='text-cyan-500 font-bold'>Now playing</h2>
        <h2 className='text-neutral-400 text-2xl'>{song.name}</h2>
        <div className='w-[240px] m-auto mt-10'>
-          <img className='w-full' src={song.links.images[0].url} />
+          <img className='w-full' src={song.links.images[1].url} />
        </div>
        <div className='flex justify-evenly items-center mt-10'>
-       <img className='w-[70px] rounded-full' src={song.links.images[1].url} />
+       <img className='w-[70px] rounded-full' src={song.links.images[0].url} />
         <span className='text-xl text-white'>{song.author}</span>
        </div>
     </div>
