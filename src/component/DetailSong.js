cc54fcd9530a27d65fbb9d24608cb73194c5f9e7
Cau <cauindo113@gmail.com>
2022-10-27 17:20:34 +0700

    fix name detail song

--- src/component/DetailSong.js
+++ src/component/DetailSong.js
@@ -6,7 +6,7 @@ export default function DetailSong() {
   return (
     <div className='col-span-5 sm:col-span-1 p-3'>
        <h2 className='text-cyan-500 font-bold'>Now playing</h2>
-       <h2 className='text-neutral-400 text-2xl'>Sing me to sleep</h2>
+       <h2 className='text-neutral-400 text-2xl'>{song.name}</h2>
        <div className='w-[240px] m-auto mt-10'>
           <img className='w-full' src={song.links.images[0].url} />
        </div>
