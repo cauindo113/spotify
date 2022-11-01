fa50b8b0b2893b29df286d7403ee849dc4b49878
Cau <cauindo113@gmail.com>
2022-11-01 11:22:20 +0700

    fvfvf

--- src/component/DetailSong.js
+++ src/component/DetailSong.js
@@ -4,11 +4,11 @@ import { Songs } from '../Context'
 export default function DetailSong() {
   const {song} = useContext(Songs)
   return (
-    <div className='col-span-5 sm:col-span-1 relative sm:h-full h-[24rem]'>
+    <div className='col-span-5 sm:col-span-1 relative sm:h-full h-[60vh]'>
        <h2 className='text-cyan-500 font-bold absolute top-0 z-10 p-3'>Now playing</h2>
        <h2 className='text-neutral-200 text-2xl absolute top-0 z-10 pl-3 pt-10'>{song.name}</h2>
        <div className='w-[100%] absolute top-0'>
-          <img className='w-full h-full brightness-50 object-cover' src={song.links.images[1].url} />
+          <img className='w-full h-[60vh] sm:h-full brightness-50 object-cover' src={song.links.images[1].url} />
        </div>
        <div className='flex justify-evenly items-center mt-10'>
        <img className='img_author w-[70px] h-[70px] rounded-full m-4 absolute bottom-0 left-0 z-10' src={song.links.images[0].url} />
