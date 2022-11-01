3d5dc89fc54deaa7782362f36d8188de3cc8a9ac
Cau <cauindo113@gmail.com>
2022-11-01 11:43:58 +0700

    hihi

--- src/component/ListSongs.js
+++ src/component/ListSongs.js
@@ -13,8 +13,8 @@ export default function ListSong() {
   }, [song])
 
   return (
-    <div className='bg-slate-800 col-span-5 sm:col-span-2 relative h-[400px] sm:h-[100%] mb-[6rem] sm:mb-0 overflow-y-scroll'>
-      <div className='sticky top-[6rem] bottom-[0rem] block h-13 bg-slate-900 p-1'>
+    <div className='table-auto w-full bg-slate-800 col-span-5 sm:col-span-2 relative h-[400px] sm:h-[100%] mb-[6rem] sm:mb-0 overflow-y-scroll'>
+      <div className='sticky top-0 z-30 bottom-0 w-full block h-13 bg-slate-900 p-1'>
       <form className="flex items-center">   
     <label for="voice-search" className="sr-only">Search</label>
     <div className="relative w-full">
